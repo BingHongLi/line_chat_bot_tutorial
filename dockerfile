a8ea51d79732942a7603f7d21878c8009a9787b8
BingHongLi <asclepius-demo-at-747862015243>
2018-02-18 12:56:34 +0800

    add some material

--- dockerfile
+++ dockerfile
@@ -1,2 +1,2 @@
 FROM jupyter/base-notebook
-RUN pip install flask requests line-bot-sdk
+RUN pip install Flask requests line-bot-sdk
--- start.sh
+++ start.sh
@@ -8,10 +8,21 @@ USERNAME=binghongli
 IMAGE=line_bot_python
 version=`cat VERSION`
 
-# 必須偵測container是否已經存在，進而做判斷
-docker rm -rf line-chat-bot-jupter
-docker run --name line-chat-bot-jupyter -p 8888:8888 -p 80:5000 -v $(pwd)/material:/home/jovyan/work -d $USERNAME/$IMAGE:$version
+create_line_bot()
+{
+docker run --name line-chat-bot-jupyter -p 8888:8888 -p 80:5000 -v $(pwd)/material:/home/jovyan/work -d $USERNAME/$IMAGE:$version start-notebook.sh --NotebookApp.token=''
+docker run --name line-ngrok -d -p 4040 --link line-chat-bot-jupyter wernight/ngrok ngrok http line-chat-bot-jupyter:80
+sleep 5s
+curl $(docker port line-ngrok 4040)/api/tunnels > tunnels.json
+docker run -v $(pwd)/tunnels.json:/tmp/tunnels.json --rm  realguess/jq jq .tunnels[1].public_url /tmp/tunnels.json 
+}
 
-# 啟用ngrok
-nohup ngrok/ngrok http -region ap 80 &
-python ngrok/view_dns.py
+# 必須偵測container是否已經存在，進而做判斷
+if [ `docker ps -a |grep line-chat-bot-jupyter | wc -l`  =  0  ]; then
+  create_line_bot
+else
+  docker rm -f line-chat-bot-jupyter
+  docker rm -f line-ngrok
+  create_line_bot
+fi
+
